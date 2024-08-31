b55fda3ed5f3ccf0c413e8be47d5acf915d2e155
Tianyu Han <petertianyuhan@hotmail.com>
2024-08-31 12:31:33 +0200

    add hest exp

---
+++ hest_exp.py
@@ -0,0 +1,8 @@
+from templates import *
+
+if __name__ == '__main__':
+    # 256 requires 8x v100s, in our case, on two nodes.
+    # do not run this directly, use `sbatch run_ffhq256.sh` to spawn the srun properly.
+    gpus = [0, 1, 2]
+    conf = hest256_autoenc()
+    train(conf, gpus=gpus)
